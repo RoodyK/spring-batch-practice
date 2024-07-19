b52d76e0d1b811352eb923da47a5fd99e685a1fa
RoodyK <pps8853@gmail.com>
2024-07-19 14:37:55 +0900

    Chunk 지향 처리 Job 작성, README 작성
    Chunk 지향 처리예제를 위한 코드 작성
    README.md 파일에 패키지 설명 및 블로그 링크 첨부

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spring Batch Practice
+
+스프링 배치 학습용 프로젝트
+
+이 프로젝트는 예제 코드와 함께 블로그에 포스팅을 했습니다.
+
+## Job 패키지
+hello.spring.batch.config 패키지 하위에 JobConfig 파일들을 작성
+
+## 블로그 링크
+- [스프링 배치 개념 및 사용](https://dawncode.tistory.com/9)
+- [스프링 배치 메타데이터 스키마](https://dawncode.tistory.com/10)
+- [스프링 배치 Chunk Oriented Processing](https://dawncode.tistory.com/11)
---
+++ src/main/java/hello/spring/batch/config/ChunkJobConfig.java
@@ -0,0 +1,82 @@
+package hello.spring.batch.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+@Slf4j
+public class ChunkJobConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job noticeJob() {
+        return jobBuilderFactory.get("chunkJob")
+                .incrementer(new RunIdIncrementer())
+                .start(noticeStep())
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step noticeStep() {
+        return stepBuilderFactory.get("chunkStep")
+                .<String, String>chunk(5)
+                .reader(noticeItemReader())
+                .processor(noticeItemProcessor())
+                .writer(noticeItemWriter())
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public ItemReader<String> noticeItemReader() {
+        log.info("[noticeItemReader] Item Read");
+        return new ListItemReader<>(getItems());
+    }
+
+    @StepScope
+    @Bean
+    public ItemProcessor<String, String> noticeItemProcessor() {
+        return item -> {
+            log.info("[noticeItemProcessor] Item Process");
+            return "Updated " + item;
+        };
+    }
+
+    @StepScope
+    @Bean
+    public ItemWriter<String> noticeItemWriter() {
+        return list -> {
+            list.forEach(item -> log.info("[noticeItemWriter] Item Write. Item = {}", item));
+        };
+    }
+
+    private List<String> getItems() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.add("Item" + i);
+        }
+
+        return list;
+    }
+}
+
